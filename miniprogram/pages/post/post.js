70e6cc5b33595506a4744977d84163fd8ea68707
Kenny Cho <kennycho168@outlook.com>
2020-04-22 23:46:55 +0800

    Home update, post page init
    Added ids to upvote & downvote buttons, added header-bar & search companents, initialized post page

--- miniprogram/pages/post/post.js
+++ miniprogram/pages/post/post.js
@@ -1,31 +1,11 @@
-// miniprogram/pages/home/home.js
+// miniprogram/pages/post/post.js
 Page({
 
   /**
    * Page initial data
    */
   data: {
-    posts: [{
-      title: "Interesting Title 1",
-      body: "Body text here- Lorem ipsum joihg kjhou oausdh oij kjhwo hsoduhf ouhl jknalkl oaihnfi- lngldm",
-      votes: 420,
-      id: "0000"
-    }, {
-        title: "Interesting Title 2",
-        body: "More body text here- asdjfha aldsfjasdlf asfj sidjjkasg asfdulhowier",
-        votes: 69,
-        id: "0001"
-      }, {
-        title: "Interesting Title 3",
-        body: "More body text here- asdjfha aldsfjasdlf asfj sidjjkasg asfdulhowier",
-        votes: 0,
-        id: "0002"
-      },{
-        title: "Interesting Title 4",
-        body: "More body text here- asdjfha aldsadjjkasg asfdulhowier",
-        votes: -20,
-        id: "0003"
-      }]
+
   },
 
   /**
