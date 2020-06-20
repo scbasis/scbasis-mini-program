0129846aeaacec108dac43ffc6e45ee0a73d4c88
2underEST <wfbyoz@gmail.com>
2020-06-20 22:37:23 +0800

    I think I am done with the new post back as well

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -7,26 +7,30 @@ Page({
    * Page initial data
    */
   data: {
-    loadnuml: appInstance.loadnum,
     posts: []
+  },
+
+  loadMore: function(){
+    const db = wx.cloud.database('scbasiscloud')
+    var that = this
+    var loadnuml = appInstance.loadnum
+    db.collection('posts').skip(loadnuml).limit(20).get({
+      success: function(res){     
+        console.log(res.data)
+        that.setData({
+          posts: that.data.posts.concat(res.data)
+        })
+        console.log(that.data.posts)
+      }
+    })
+    appInstance.loadnum = loadnuml + 20
+    console.log(appInstance.loadnum)
   },
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-    const db = wx.cloud.database('scbasiscloud')
-    var that = this
-    db.collection('posts').skip(this.data.loadnuml).limit(20).get({
-      success: function(res){     
-        console.log(res.data)
-        that.setData({
-          posts: res.data
-        })
-        console.log(that.data.posts)
-      }
-    })
-    appInstance.loadnum = this.data.loadnuml + 20
-    console.log(appInstance.loadnum)
+    loadMore()
   },
 
   /**
@@ -68,19 +72,7 @@ Page({
    * Called when page reach bottom
    */
   onReachBottom: function () {
-    const db = wx.cloud.database('scbasiscloud')
-    var that = this
-    db.collection('posts').skip(this.data.loadnuml).limit(20).get({
-      success: function(res){     
-        console.log(res.data)
-        that.setData({
-          posts: res.data
-        })
-        console.log(that.data.posts)
-      }
-    })
-    appInstance.loadnum = this.data.loadnuml + 20
-    console.log(appInstance.loadnum)
+    loadMore()
   },
 
   /**
--- miniprogram/pages/new post/new post.js
+++ miniprogram/pages/new post/new post.js
@@ -5,7 +5,8 @@ Page({
    * Page initial data
    */
   data: {
-
+    title: '',
+    content: ''
   },
 
   /**
@@ -64,5 +65,36 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /***
+   * Called when user enters the title
+   */
+  getTitleData: function(event){
+    this.setData({
+      title: event.detail.value
+    })
+    console.log(this.data.title)
+  },
+  getContentData: function(event){
+    this.setData({
+      content: event.detail.value
+    })
+    console.log(this.data.content)
+  },
+  createPost: function(event){
+    const db = wx.cloud.database('scbasiscloud')
+    var that = this
+    var loadnuml = appInstance.loadnum
+    db.collection('posts').add({
+      data: {
+        title: that.data.title,
+        body: that.data.content,
+        votes: 0,
+        id: 0,
+        upvoted: false,
+        downvoted: false
+      }
+    })
   }
 })
