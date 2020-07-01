07726e70a08933b1b49337f2a89d48692feda11e
crocodile27 <anthea.guo@hotmail.com>
2020-07-01 10:25:33 +0800

    New Category Page
    Shows categories created by users and has top 3 discussions under each one.

---
+++ miniprogram/pages/category/category.js
@@ -0,0 +1,71 @@
+// miniprogram/pages/category/category.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
+
+function showDiscussions(cat_id){
+  var wxss_id = "cat_" + cat_id;
+  $(wxss_id).wxss("display","block");//need help figuring out this part, on css the wxss is css, however it doesn't seem to recognize wxss...
+}
