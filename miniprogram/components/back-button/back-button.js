5d0dc665c97751e936e5b5d41a882c1245e45c15
Kenny Cho <kennycho168@outlook.com>
2020-08-16 21:20:46 +0800

    Added back-button components
    Made the "Back" buttons in the header-bar components to standardize style
    
    Though it might be obsolete cuz of the wechat default back arrow thing...

---
+++ miniprogram/components/back-button/back-button.js
@@ -0,0 +1,26 @@
+// components/back-button/back-button.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    url: {
+      type: String,
+      value: "../home/home"
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
