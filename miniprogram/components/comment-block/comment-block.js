2086808105845f5f1137692c44042ce2df3c37c1
Kenny Cho <kennycho168@outlook.com>
2020-08-24 15:38:10 +0800

    Comment collapsing with animation
    Comments now collapses with a 0.2 transition/animation. Achieved using css transition and js setData to update the height of the children-container

--- miniprogram/components/comment-block/comment-block.js
+++ miniprogram/components/comment-block/comment-block.js
@@ -20,14 +20,16 @@ Component({
   data: {
     collapsed: false,
     display: "block",
-    childrenHeight: 0
+    childrenHeight: 0,
+    height: "fit-content",
+    heightValue: 0
   },
   /**
    * Component lifecycle functions
    */
   lifetimes: {
     attached: function() {
-      // this.getChildrenHeight();
+      this.getChildrenHeight();
     }
   },
 
@@ -38,22 +40,60 @@ Component({
   methods: {
 
     getChildrenHeight: function () { // not working: returns null for res
-      wx.createSelectorQuery().select('.children-container').boundingClientRect((res) => {
-        console.log(res);
+      wx.createSelectorQuery().in(this).select('.children-container').boundingClientRect((res) => {
+        // console.log(res);
         var childrenHeight = res.height;
-        this.setData({"childrenHeight": childrenHeight});
+        this.setData({
+          "childrenHeight": childrenHeight, 
+          "heightValue": childrenHeight,
+          "height": childrenHeight + "px"
+        });
       }).exec();
     },
 
     collapse: function(ev) {
+      if (this.properties.comment.children.length == 0) return;
       this.data.collapsed = !this.data.collapsed;
+      
       // console.log("collapse() called", ev);
       if (this.data.collapsed) {
-        this.setData({"display": "none"});
+        this.triggerEvent("collapse", {
+          heightChange: -1 * this.data.childrenHeight
+        }, {
+          bubbles: true
+        })
+        this.setData({
+          height: "0px",
+          heightValue: 0
+        });
       } else {
-        this.setData({"display": "block"});
+        this.triggerEvent("collapse", {
+          heightChange: this.data.childrenHeight
+        }, {
+          bubbles: true
+        })
+        this.setData({
+          heightValue: this.data.childrenHeight,
+          height: this.data.childrenHeight + "px"
+        });
       }
       // console.log(this.data.display);
+      
+    },
+
+    handleChildrenCollapse: function(e) {
+      var heightChange = e.detail.heightChange
+      // console.log(this.data.childrenHeight, heightChange)
+      this.setData({
+        childrenHeight: this.data.childrenHeight + heightChange
+      }, () => {
+        if (!this.data.collapsed) {
+          this.setData({
+            heightValue: this.data.childrenHeight,
+            height: this.data.childrenHeight + "px"
+          })
+        }
+      })
       
     }
   }
