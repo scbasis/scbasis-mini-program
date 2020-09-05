84d70c043462db581f8953b3489db091a863d860
crocodile27 <anthea.guo@hotmail.com>
2020-09-05 19:25:51 +0800

    Notification basic outline

---
+++ miniprogram/components/notification-block/notification-block.js
@@ -0,0 +1,23 @@
+// components/notification-block/notification-block.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
